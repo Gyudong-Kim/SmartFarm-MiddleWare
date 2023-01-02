328c9314f0c95293428572d2fdd9f2326feb8f25
Gyudong-Kim <dong3955@gmail.com>
2023-01-02 15:59:53 +0900

    log에 Datetime 추가

--- routes/control.js
+++ routes/control.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const moment = require("moment");
 const router = express.Router();
 const filterService = require("../services/filter-service");
 
 router.post("/code", function (req, res, next) {
+    let time = moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:00");
+
+    console.info(`\n<${time}>`);
     console.info(`Data from server => ${JSON.stringify(req.body)}`);
     try {
         filterService.filter(req.body);
